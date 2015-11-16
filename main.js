b311518e8b352738db0dd231f00d68d04273668f
Yuji Nobuke <nobunobucarp@gmail.com>
2015-11-16 12:42:43 +0900

    my-id

--- main.js
+++ main.js
@@ -21,6 +21,14 @@ var commanderURL ="./Commander.php";
 //接続
 var peer = new Peer({ key: '2e8076d1-e14c-46d4-a001-53637dfee5a4', debug: 3});
 peer.on('open', function(){
+    $("my-id").text(peer.id);
+    $.ajax({
+        url: "http://nobuta.xyz/skyway/center/ID.php?peerid="+peer.id,
+            type: "GET",
+            dataType: "html"
+    }).done(function(res) {
+        $('#my-number').text(res);
+    });
 });
 peer.on('call', function(call){ //かかってきたとき
     calledDo(call);
@@ -30,28 +38,29 @@ peer.on('connection',function(conn){    //接続されたとき
 });
 $(function() {  //能動的に動く部分
     navigator.getUserMedia({audio: false, video: true}, function(stream){
-        localStream = stream;
+       // localStream = stream;
         var url = URL.createObjectURL(stream);
-        $('#my-video').prop('src', url);
+        //$('#my-video').prop('src', url);
+
     }, function() { alert("Error!"); });
     $('#call-start').click(function(){  //コネクションボタン押した
-        connectedNum++;
-        var peer_id = $('#peer-id-input').val(); //相手のID
-        connectedCall[connectedNum] = peer.call(peer_id, localStream);//これで接続
-        connectedConn[connectedNum] = peer.connect(peer_id);//これでデータコネクション接続
+        //connectedNum++;
+        //var peer_id = $('#peer-id-input').val(); //相手のID
+        //connectedCall[connectedNum] = peer.call(peer_id, localStream);//これで接続
+        //connectedConn[connectedNum] = peer.connect(peer_id);//これでデータコネクション接続
        // connectedCall=call; connectedConn=conn; //グローバル変数に記録
         calledDo();
         connectedDo(); //接続したあとにデータのやりとり
     });
     $("#sender").click(function(){ //送信
         var selected = $("input[name=submitNum]:checked").val();
-        connectedConn[selected].send($("#send-data").val());
+        //connectedConn[selected].send($("#send-data").val());
     });
     $('#call-end').click(function(){ //終了
         var selected = $("input[name=closeNum]:checked").val();
-        connectedCall[selected].close();
-        connectedConn[selected].close();
-        connectedNum--;
+        //connectedCall[selected].close();
+        //connectedConn[selected].close();
+        //connectedNum--;
         $("#peer-id"+connectedNum).text("");
     });
 });
